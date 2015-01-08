b54c2c72055e3f3d285c080ee9d622e7ca7addb9
Rafa de Castro <rafael@micubiculo.com>
2015-01-08 10:27:22 +0100

    Installed flycheck

---
+++ elpa/flycheck-0.22/flycheck-pkg.el
@@ -0,0 +1,12 @@
+(define-package "flycheck" "0.22" "Modern on-the-fly syntax checking for GNU Emacs"
+  '((dash "2.4.0")
+    (pkg-info "0.4")
+    (let-alist "1.0.1")
+    (cl-lib "0.3")
+    (emacs "24.1"))
+  :keywords
+  '("convenience" "languages" "tools")
+  :url "https://www.flycheck.org")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

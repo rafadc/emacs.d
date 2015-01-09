cc980b2dd1c3d0e5632ce25b09812a2921f8c393
Rafa de Castro <rafael@micubiculo.com>
2015-01-09 21:24:13 +0100

    Using helm for kill ring

--- src/helm.el
+++ src/helm.el
@@ -2,5 +2,7 @@
 
 (global-set-key (kbd "C-x b") 'helm-buffers-list) ; Helm for buffer list
 
+(global-set-key (kbd "M-y") 'helm-show-kill-ring) ; Helm for kill ring
+
 (global-set-key (kbd "s-p") 'helm-projectile)
 

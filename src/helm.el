ad04617a4a22bef7e681868cd961a86145b1eb61
Rafa de Castro <rafael@micubiculo.com>
2015-01-09 20:13:54 +0100

    Using helm for buffer list also

--- src/helm.el
+++ src/helm.el
@@ -1,4 +1,6 @@
-(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "M-x") 'helm-M-x) ; Helm for emacs commands
+
+(global-set-key (kbd "C-x b") 'helm-buffers-list) ; Helm for buffer list
 
 (global-set-key (kbd "s-p") 'helm-projectile)
 

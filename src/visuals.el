e1e58b994a3327b628364efa30ffe200a5662b1f
Rafa de Castro <rafael@micubiculo.com>
2015-01-09 20:07:32 +0100

    Improving scrolling

--- src/visuals.el
+++ src/visuals.el
@@ -15,3 +15,14 @@
 ;; Font
 (set-face-attribute 'default nil
                     :family "M+ 2m" :height 180 :weight 'light)
+
+;; Smoother scrolling with mouse
+(setq mouse-wheel-follow-mouse 't)
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1)))
+
+;; Scrolling with keyboard before touching bottom
+(setq redisplay-dont-pause t
+      scroll-margin 1
+      scroll-step 1
+      scroll-conservatively 10000
+      scroll-preserve-screen-position 1)

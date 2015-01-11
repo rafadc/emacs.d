77e4ede079c66cccebdc26002b5e487a2efa1604
Rafa de Castro <rafael@micubiculo.com>
2015-01-11 16:02:31 +0100

    Function of grabbing text down is working now

--- src/moving_text.el
+++ src/moving_text.el
@@ -1,1 +1,11 @@
-(global-set-key (kbd "M-<down>") 'transpose-lines)
+(defun grab-line-down ()
+  "Moves current line down"
+  (interactive)
+  (progn
+   (forward-line 1)
+   (transpose-lines 1)
+   (forward-line -1)))
+
+(global-set-key (kbd "M-<down>") 'grab-line-down)
+
+

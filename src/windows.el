d0686be1d87c5cfa465f064eeb94fc4a8744400f
Rafa de Castro <rafael@micubiculo.com>
2015-01-11 15:04:53 +0100

    Making possible to invoke function from minibuffer without params
    Overkill... YES! But I'm learning.

--- src/windows.el
+++ src/windows.el
@@ -1,8 +1,10 @@
 (global-set-key (kbd "<M-s-right>") 'other-window)
 
-(defun other-window-previous (n)
+(defun other-window-previous (&optional n)
   "Moves to previous window"
   (interactive "p")
-  (other-window (- n))
+  (if n
+      (other-window (- n))
+      (other-window -1)))
 
 (global-set-key (kbd "<M-s-left>") 'other-window-previous)

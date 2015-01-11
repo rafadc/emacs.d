3867eaf5853648e4923c5ae565474914b1cb64f1
Rafa de Castro <rafael@micubiculo.com>
2015-01-11 16:05:57 +0100

    Now function for grabbing text up is also working

--- src/moving_text.el
+++ src/moving_text.el
@@ -6,6 +6,16 @@
    (transpose-lines 1)
    (forward-line -1)))
 
+(defun grab-line-up ()
+  "Moves current line up"
+  (interactive)
+  (progn
+   (forward-line 1)
+   (transpose-lines -1)
+   (forward-line -2)))
+
 (global-set-key (kbd "M-<down>") 'grab-line-down)
+(global-set-key (kbd "M-<up>") 'grab-line-up)
+
 
 

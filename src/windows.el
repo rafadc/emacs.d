1325b53d53b91ab55ae19e5a5e45f87054151e46
Rafa de Castro <rafael@micubiculo.com>
2015-01-11 15:06:46 +0100

    Easier to read now

--- src/windows.el
+++ src/windows.el
@@ -3,8 +3,6 @@
 (defun other-window-previous (&optional n)
   "Moves to previous window"
   (interactive "p")
-  (if n
-      (other-window (- n))
-      (other-window -1)))
+  (other-window (if n (- n) -1)))
 
 (global-set-key (kbd "<M-s-left>") 'other-window-previous)

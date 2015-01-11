61e5b189cffb5369c6facaf2d5373ab0cf43c7e7
Rafa de Castro <rafael@micubiculo.com>
2015-01-11 14:55:24 +0100

    Added previous window function and Sublime like binding

--- src/windows.el
+++ src/windows.el
@@ -1,1 +1,8 @@
 (global-set-key (kbd "<M-s-right>") 'other-window)
+
+(defun other-window-previous ()
+  "Moves to previous window"
+  (interactive)
+  (other-window -1))
+
+(global-set-key (kbd "<M-s-left>") 'other-window-previous)

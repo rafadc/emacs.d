7d40278c25147d4d3dbc67f0d2e2d062fc55f804
Rafa de Castro <rafael@micubiculo.com>
2015-01-17 22:19:54 +0100

    This binding seems to be more comfortable for navigating

--- src/windows.el
+++ src/windows.el
@@ -1,8 +1,8 @@
-(global-set-key (kbd "<M-s-right>") 'other-window)
+(global-set-key (kbd "<C-x p>") 'other-window)
 
 (defun other-window-previous (&optional n)
   "Moves to previous window"
   (interactive "p")
   (other-window (if n (- n) -1)))
 
-(global-set-key (kbd "<M-s-left>") 'other-window-previous)
+(global-set-key (kbd "<C-x o>") 'other-window-previous)

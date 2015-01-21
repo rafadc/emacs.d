423ceff98dd3801e218a00a931c8474ced25e6ca
Rafa de Castro <rafael@micubiculo.com>
2015-01-21 23:14:43 +0100

    I don't know why this could work

--- src/org-mode.el
+++ src/org-mode.el
@@ -1,13 +1,13 @@
 ;; Binding F7 to open a personal_notes.org file in root of projectile
 (require 'projectile)
 
-(defun open-todoorg ()
+(defun projectile-open-personal-notes ()
   "Opens a personal_notes.org file in project folder"
   (interactive)
   (let
-      ((folder (first (projectile-get-project-directories))))
+      ((folder (car (projectile-get-project-directories))))
     (if folder
       (find-file (concat folder "personal_notes.org"))
       (message "No project folder found"))))
 
-(global-set-key (kbd "<f7>") 'open-todoorg)
+(global-set-key (kbd "<f7>") 'projectile-open-personal-notes)

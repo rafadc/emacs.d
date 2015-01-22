d78a87022939bb549dbc0de2a643c1fbf35dbd2b
Rafa de Castro <rafael@micubiculo.com>
2015-01-22 12:39:01 +0100

    Enabled flymake for Ruby

--- src/languages/ruby.el
+++ src/languages/ruby.el
@@ -1,5 +1,11 @@
+;; Adding file types with no rb extension
 (add-to-list 'auto-mode-alist
 	     '("\\.\\(?:gemspec\\|irbrc\\|gemrc\\|rake\\|rb\\|ru\\|thor\\)\\'" . ruby-mode))
 
 (add-to-list 'auto-mode-alist
                '("\\(Capfile\\|Gemfile\\(?:\\.[a-zA-Z0-9._-]+\\)?\\|[rR]akefile\\)\\'" . ruby-mode))
+
+
+;; Adding syntax checking
+(add-hook 'ruby-mode-hook 'flymake-ruby-load)
+

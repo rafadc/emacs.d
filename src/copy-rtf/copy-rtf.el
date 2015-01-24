e3910f92c55607f30ff4c67852ca6f32145582b0
Rafa de Castro <rafael@micubiculo.com>
2015-01-24 07:46:45 +0100

    Added themes to script to copy as rtf

--- src/copy-rtf/copy-rtf.el
+++ src/copy-rtf/copy-rtf.el
@@ -1,5 +1,17 @@
 ;; Copy code as RTF to the clipboard. This is commonly used for 
 ;; presentations in keynote or such
+;;
+;; This is compatible only with MacOSX
+;;
+;; You need to first: brew install highlight
+;;
+;; To change theme you just need to
+;; (setq copy-as-rtf-theme "theme")
+;;
+;; Themes are: andes, breeze, candy, fine-blue, matrix, moria, seashell
+;;             vampire, zellner, zenburn
+
+(setq copy-as-rtf-theme "zenburn")
 
 (defun copy-as-rtf (start end)
   (interactive "r")
@@ -7,7 +19,8 @@
 	(temp-file (make-temp-file "copy-as-rtf"))
 	(file-format (file-name-extension (buffer-file-name)))
 	(selected-text (buffer-substring start end))
-	(command (concat "highlight --out-format=rtf --src-lang " file-format " -i " temp-file " | pbcopy "))
+	(options (concat "--out-format=rtf --src-lang " file-format " --style " copy-as-rtf-theme " "))
+	(command (concat "highlight " options " -i " temp-file " | pbcopy "))
 	)
     (append-to-file start end temp-file)
     (shell-command command)

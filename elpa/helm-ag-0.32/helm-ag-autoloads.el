34d2c4b1f3c4a6e1f916712243b39629828fdc11
Rafa de Castro <rafael@micubiculo.com>
2015-01-29 10:52:36 +0100

    Upgraded helm-ag

--- elpa/helm-ag-0.32/helm-ag-autoloads.el
+++ elpa/helm-ag-0.32/helm-ag-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "helm-ag" "helm-ag.el" (21697 14099 0 0))
+;;;### (autoloads nil "helm-ag" "helm-ag.el" (21706 721 0 0))
 ;;; Generated autoloads from helm-ag.el
 
 (autoload 'helm-ag-pop-stack "helm-ag" "\
@@ -30,11 +30,6 @@
 
 
 \(fn &optional BASEDIR)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("helm-ag-pkg.el") (21697 14099 672114
-;;;;;;  0))
 
 ;;;***
 

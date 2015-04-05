799ba6c86131df7b9e42d8efb8860f8efbbb0d2a
Rafa de Castro <rafael@micubiculo.com>
2015-04-05 19:25:47 +0200

    Moved windows.el to settings.org

--- src/windows.el
+++
@@ -1,8 +0,0 @@
-(global-set-key (kbd "C-x p") 'other-window)
-
-(defun other-window-previous (&optional n)
-  "Moves to previous window"
-  (interactive "p")
-  (other-window (if n (- n) -1)))
-
-(global-set-key (kbd "C-x o") 'other-window-previous)

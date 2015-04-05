4dd819673833d4e3e75a2515616c59dfa7f60f04
Rafa de Castro <rafael@micubiculo.com>
2015-04-05 20:34:59 +0200

    Moved programming languages to settings.org

--- src/languages/haskell.el
+++
@@ -1,2 +0,0 @@
-(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
-(add-hook 'haskell-mode-hook 'flycheck-mode)
--- src/languages/htmlcss.el
+++
@@ -1,4 +0,0 @@
-(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-
-(setq web-mode-markup-indent-offset 2)
-(setq web-mode-css-indent-offset 2)

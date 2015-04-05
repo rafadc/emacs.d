801b13032a58ce95411e8b7c131ff39c80b70f86
Rafa de Castro <rafael@micubiculo.com>
2015-04-05 11:37:41 +0200

    Two hours was to little to clean buffers.

--- src/midnight.el
+++ src/midnight.el
@@ -6,10 +6,9 @@
 (defvar clean-buffer-list-timer nil
   "Stores clean-buffer-list timer if there is one. You can disable clean-buffer-list by (cancel-timer clean-buffer-list-timer).")
 
-;; run clean-buffer-list every 2 hours
-(setq clean-buffer-list-timer (run-at-time t 7200 'clean-buffer-list))
+;; run clean-buffer-list every 4 hours
+(setq clean-buffer-list-timer (run-at-time t 14400 'clean-buffer-list))
 
 ;; kill everything, clean-buffer-list is very intelligent at not killing
 ;; unsaved buffer.
 (setq clean-buffer-list-kill-regexps '("^.*$"))
-

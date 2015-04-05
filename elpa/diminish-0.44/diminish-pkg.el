bcca75d4e487379f6f9b040d2fae552502ecc61f
Rafa de Castro <rafael@micubiculo.com>
2015-04-05 09:40:43 +0200

    Installed use-package

---
+++ elpa/diminish-0.44/diminish-pkg.el
@@ -0,0 +1,1 @@
+(define-package "diminish" "0.44" "Diminished modes are minor modes with no modeline display" 'nil :url "http://www.eskimo.com/~seldon" :keywords '("extensions" "diminish" "minor" "codeprose"))

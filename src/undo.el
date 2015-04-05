2dc28e2394e8b0ca8950990b2509386a9b9ce2b6
Rafa de Castro <rafael@micubiculo.com>
2015-04-05 19:28:25 +0200

    Moved undo to settings.org

--- src/undo.el
+++
@@ -1,8 +0,0 @@
-(global-undo-tree-mode 1)
-
-(defalias 'redo 'undo-tree-redo)
-
-(global-set-key (kbd "s-z") 'undo)
-(global-set-key (kbd "s-Z") 'redo)
-(global-set-key (kbd "<C-s-268632090>") 'undo-tree-visualize)
-

191ddc8a6e7025eb28c834e550c6ab4f236f5480
Rafa de Castro <rafael@micubiculo.com>
2015-04-05 20:38:25 +0200

    Moved dash integration into settings.org

--- src/dash.el
+++
@@ -1,7 +0,0 @@
-;; Send to Dash
-(add-to-list 'load-path "~/.emacs.d/unmanaged/dash-at-point")
-(autoload 'dash-at-point "dash-at-point"
-  "Search the word at point with Dash." t nil)
-(global-set-key "\C-cd" 'dash-at-point)
-(global-set-key "\C-ce" 'dash-at-point-with-docset)
-

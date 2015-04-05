ac4412255592abb826bea2845e25d78f6cd4d4e2
Rafa de Castro <rafael@micubiculo.com>
2015-04-05 10:04:48 +0200

    Changed the folder for emacs saves so I can tab complete when entering my emacs home folder

--- src/backup-files.el
+++ src/backup-files.el
@@ -4,7 +4,7 @@
 (setq
    backup-by-copying t      ; don't clobber symlinks
    backup-directory-alist
-    '(("." . "~/.emacs-saves"))    ; don't litter my fs tree
+    '(("." . "~/.saves-emacs"))    ; don't litter my fs tree
    delete-old-versions t
    kept-new-versions 6
    kept-old-versions 2

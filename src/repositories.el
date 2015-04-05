e24925b1db186ac134e043bea2f8a4c02e375a97
Rafa de Castro <rafael@micubiculo.com>
2015-04-05 20:40:08 +0200

    Repositories are now in init.el

--- src/repositories.el
+++
@@ -1,7 +0,0 @@
-(require 'package)
-(add-to-list 'package-archives
-             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
-(add-to-list 'package-archives 
-	     '("marmalade" .
-	       "http://marmalade-repo.org/packages/"))
-(package-initialize)

cf011a14810ffb83c0f2a05b3b07c58a2e6ea7bd
Rafa de Castro <rafael@micubiculo.com>
2015-06-25 21:42:34 +0200

    Updated

--- elpa/clojure-mode-4.0.1/clojure-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "clojure-mode" "4.0.1" "Major mode for Clojure code" '((emacs "24.1")))
---
+++ elpa/elixir-mix-1.0.0/elixir-mix-pkg.el
@@ -0,0 +1,1 @@
+(define-package "elixir-mix" "1.0.0" "Emacs integration for Elixir's mix" 'nil)

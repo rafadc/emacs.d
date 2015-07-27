6ab5bb079127a8febfa5b59f409373ed157b2b2f
Rafa de Castro <rafael@micubiculo.com>
2015-07-27 08:41:42 +0200

    Updated

---
+++ elpa/alchemist-1.3.1/alchemist-pkg.el
@@ -0,0 +1,9 @@
+(define-package "alchemist" "1.3.1" "Elixir tooling integration into Emacs"
+  '((elixir-mode "2.2.5")
+    (emacs "24.4")
+    (company "0.8.0"))
+  :url "http://www.github.com/tonini/alchemist.el" :keywords
+  '("languages" "mix" "elixir" "elixirc" "hex"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

f7ff721fa80b5067bff5c865bd431784c531870f
Rafa de Castro <rafael@micubiculo.com>
2015-08-25 10:43:10 +0200

    Updated

--- elpa/async-1.3/async-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "async" "1.3" "Asynchronous processing in Emacs" 'nil)
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
--- elpa/with-editor-2.1.0/with-editor-pkg.el
+++
@@ -1,8 +0,0 @@
-(define-package "with-editor" "2.1.0" "Use the Emacsclient as $EDITOR"
-  '((emacs "24.4")
-    (dash "2.10.0"))
-  :url "https://github.com/magit/magit" :keywords
-  '("tools"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:

a704ba525c7789d791d4f840ae920e7010042e8d
Rafa de Castro <rafael@micubiculo.com>
2015-09-25 09:19:38 +0200

    Updated

---
+++ elpa/magit-2.2.2/magit-pkg.el
@@ -0,0 +1,12 @@
+(define-package "magit" "2.2.2" "A Git porcelain inside Emacs"
+  '((emacs "24.4")
+    (async "1.4")
+    (dash "2.11.0")
+    (with-editor "2.2.2")
+    (git-commit "2.2.2")
+    (magit-popup "2.2.2"))
+  :url "https://github.com/magit/magit" :keywords
+  '("git" "tools" "vc"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/magit-popup-2.2.2/magit-popup-pkg.el
@@ -0,0 +1,9 @@
+(define-package "magit-popup" "2.2.2" "Define prefix-infix-suffix command combos"
+  '((emacs "24.4")
+    (async "1.4")
+    (dash "2.11.0"))
+  :url "https://github.com/magit/magit" :keywords
+  '("bindings"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

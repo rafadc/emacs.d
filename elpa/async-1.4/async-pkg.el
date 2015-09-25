d54d96858b84b8deb0739b11de13a7e760bfd938
Rafa de Castro <rafael@micubiculo.com>
2015-09-25 09:20:16 +0200

    Updated

--- elpa/async-1.4/async-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "async" "1.4" "Asynchronous processing in Emacs" 'nil)
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
--- elpa/rake-0.3.3/rake-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "rake" "0.3.3" "Run rake commands" '((f "0.13.0") (dash "1.5.0") (cl-lib "0.5")) :url "https://github.com/asok/rake.el" :keywords '("rake" "ruby"))

fbaef69f76bfb6c3b93e920e6d72992e8611a503
Rafa de Castro <rafael@micubiculo.com>
2015-11-06 15:49:27 +0100

    Autosquasing in magi's rebases

--- init.el
+++ init.el
@@ -7,7 +7,8 @@
  '(custom-safe-themes
    (quote
     ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "756597b162f1be60a12dbd52bab71d40d6a2845a3e3c2584c6573ee9c332a66e" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" default)))
- '(magit-push-arguments (quote ("--set-upstream"))))
+ '(magit-push-arguments (quote ("--set-upstream")))
+ '(magit-rebase-arguments (quote ("--autosquash" "--autostash"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

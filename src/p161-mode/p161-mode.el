4e91e9212e698d73d465bd5b5d928ae5c2f1d7d0
Rafa de Castro <rafael@micubiculo.com>
2015-11-16 23:45:00 +0100

    Fixed the auto insertion of ticket name

--- src/p161-mode/p161-mode.el
+++ src/p161-mode/p161-mode.el
@@ -1,8 +1,3 @@
-(defvar p161-mode nil
-  "Mode for Platform 161")
-
-(make-variable-buffer-local 'p161-mode)
-
 (defun git-folder ()
   "Returns the folder for the project"
   (car (projectile-get-project-directories)))
@@ -15,20 +10,6 @@
        (ticket-name (car (split-string branch-name "\\."))))
     (if (string-match "PFM-" ticket-name)
       ticket-name)))
-
-(defun toggle-p161 ()
-  "Toggles p161 mode based on input parameters to the function to follow standard minor mode"
-  (if (null arg)
-      (if p161-mode
-          (not p161-mode)
-        (> (prefix-numeric-value arg) 0))))
-
-(defun add-p161-mode-to-alist ()
-  "Add the p161 mode to the minor mode alist if it is not there"
-  (if (not (assq 'p161-mode minor-mode-alist))
-      (setq minor-mode-alist
-            (cons '(p161-mode " P161")
-                  minor-mode-alist))))
 
 (defun open-current-ticket-in-browser ()
   "Opens the current ticket in a browser"
@@ -45,22 +26,10 @@
   "Asks for a ticket and creates a branch for it starting in the current branch."
   (interactive "P\nbTicket number: ")
   (let*
-      ((ticket-text "")
+      ((ticket-text "sample-ticket-text")
        (branch-name (concat ticket-number ".rafa." ticket-text)))
     (progn
       (shell-command-to-string (concat "git checkout -b" branch-name)))))
-
-(defun add-shortcuts ()
-  "Adds all shortcuts to mode"
-  (progn
-      (global-set-key (kbd "C-c p c") 'open-current-ticket-in-browser)
-      (global-set-key (kbd "C-c p n") 'create-branch-for-ticket)))
-
-(defun remove-shortcuts ()
-  "Removes all shortcuts from mode"
-  (progn
-      (global-unset-key (kbd "C-c p c"))
-      (global-unset-key (kbd "C-c p n"))))
 
 (defun insert-pfm-in-commit-message ()
   "Inserts the PFM ticket name at the beginning of commit message"
@@ -69,16 +38,19 @@
       ((git-folder (car (projectile-get-project-directories)))
        (command (concat "git --git-dir=" git-folder ".git branch | sed -n '/* /s///p'"))
        (branch-name (shell-command-to-string command))
-       (ticket-name (split-string branch-name "\\.")))
-    (if ticket-name
-      (insert ticket-name))))
+       (ticket-name (car (split-string branch-name "\\."))))
+    (if (string-match-p (regexp-quote "PFM-") ticket-name)
+        (insert (concat ticket-name " ")))))
 
-(defun p161-mode (&optional arg)
+(define-minor-mode p161-mode
   "Platform 161 mode"
-  (interactive "P")
-  (setq p161-mode (toggle-p161))
-  (if p161-mode
-      (progn
-        (add-shortcuts)
-        (add-hook 'git-commit-mode-hook 'insert-pfm-in-commit-message))
-    (remove-shortcuts)) )
+  :lighter " P161"
+  :global t
+  :keymap (let ((map (make-sparse-keymap)))
+            (define-key map (kbd "C-c p c") 'create-branch-for-ticket)
+            (define-key map (kbd "C-c p n") 'open-current-ticket-in-browser)
+            map)
+  (add-hook 'git-commit-mode-hook 'insert-pfm-in-commit-message)
+  )
+
+(provide 'p161-mode)

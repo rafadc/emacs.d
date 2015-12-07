98b6ddc477dbe0b1b785c828cb2796fd0bae013b
Rafa de Castro <rafael@micubiculo.com>
2015-12-07 16:26:22 +0100

    Moved feeds to their own file

---
+++ elfeed/feeds.el
@@ -0,0 +1,6 @@
+(setq elfeed-feeds
+      '(("http://feeds.feedburner.com/codinghorror" programming) ; Coding Horror
+        ("http://www.toptal.com/blog.rss" programming)           ; Toptal
+        ("http://xkcd.com/rss.xml" webcomic)                     ; XKCD
+        ("http://sachachua.com/blog/feed/" emacs)                ; Sacha chua
+        ))

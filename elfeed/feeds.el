fa13f5d8ee686998df35ca03d5b3101d384593f5
Rafa de Castro <rafael@micubiculo.com>
2016-04-19 20:09:49 +0200

    Added elfeed-org

--- elfeed/feeds.el
+++
@@ -1,8 +0,0 @@
-(setq elfeed-feeds
-      '(("http://feeds.feedburner.com/codinghorror" programming) ; Coding Horror
-        ("http://www.toptal.com/blog.rss" programming)           ; Toptal
-        ("http://martinfowler.com/feed.atom" programming)        ; Martin Fowler
-        ("http://xkcd.com/rss.xml" webcomic)                     ; XKCD
-        ("http://sachachua.com/blog/feed/" emacs)                ; Sacha chua
-        ("http://nullprogram.com/feed/" emacs)                   ; Null program
-        ))
